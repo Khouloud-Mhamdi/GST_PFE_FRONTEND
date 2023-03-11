b68d9782e32fed8315661d4d26923225b990aae1
MayssaHkiri <may26saa@gmail.com>
2023-03-11 12:50:04 +0100

    integration du template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { IntroSectionComponent } from './components/intro-section/intro-section.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    IntroSectionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
