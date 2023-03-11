c641a4b86148debdf985cee8c0de8b871b5cb963
khouloud <khouloudmhamdi7@gmail.com>
2023-03-11 18:00:03 +0100

    home page and disciplines

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasketPageComponent } from './components/basket-page/basket-page.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { HandballPageComponent } from './components/handball-page/handball-page.component';
+import { HomeComponent } from './components/home/home.component';
+import { KaratePageComponent } from './components/karate-page/karate-page.component';
+import { TennisPageComponent } from './components/tennis-page/tennis-page.component';
+import { VolleyballPageComponent } from './components/volleyball-page/volleyball-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : "" , component:HomeComponent},
+  {path : "Contact" , component:ContactPageComponent},
+  {path : "Basketball" , component:BasketPageComponent},
+  {path : "Tennis" , component:TennisPageComponent},
+  {path : "Volleyball" , component:VolleyballPageComponent},
+  {path : "Karaté" , component:KaratePageComponent},
+
+  {path : "Handball" , component:HandballPageComponent},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,41 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { IntroSectionComponent } from './components/intro-section/intro-section.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { InfoSectionComponent } from './components/info-section/info-section.component';
+import { DisciplinesSectionComponent } from './components/disciplines-section/disciplines-section.component';
+import { EventsSectionComponent } from './components/events-section/events-section.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { BasketPageComponent } from './components/basket-page/basket-page.component';
+import { AproposSectionComponent } from './components/apropos-section/apropos-section.component';
+
+import { TennisPageComponent } from './components/tennis-page/tennis-page.component';
+import { HandballPageComponent } from './components/handball-page/handball-page.component';
+import { VolleyballPageComponent } from './components/volleyball-page/volleyball-page.component';
+import { KaratePageComponent } from './components/karate-page/karate-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
 
+    AppComponent,
+    FooterComponent,
+    IntroSectionComponent ,
     HeaderComponent,
-    HomeComponent ,
+    HomeComponent,
+    InfoSectionComponent,
+    DisciplinesSectionComponent,
+    EventsSectionComponent,
+    ContactPageComponent,
+    BasketPageComponent,
+    AproposSectionComponent,
+
+    TennisPageComponent,
+    HandballPageComponent,
+    VolleyballPageComponent,
+    KaratePageComponent ,
 ],
   imports: [
     BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
