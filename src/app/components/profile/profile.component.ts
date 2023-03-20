a3d9e6be8b9e09fba76a4216d333e4863c30286c
MayssaHkiri <may26saa@gmail.com>
2023-03-20 15:41:16 +0100

    show and edit profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 import { TokenStorageService } from 'src/app/services/token-storage.service';
 
 @Component({
@@ -8,10 +10,49 @@ import { TokenStorageService } from 'src/app/services/token-storage.service';
 })
 export class ProfileComponent implements OnInit {
   currentUser: any;
-  constructor(private token: TokenStorageService) { }
+  user : any = {}; 
+  showConfirmationDialog = false ; 
+  update = false ; 
+  erreur = false ; 
+  constructor(private token: TokenStorageService , private authService : AuthService , private router : Router) { }
 
   ngOnInit(): void {
     this.currentUser = this.token.getUser();
+    console.log(this.currentUser.id);
+    this.authService.getCurrentUserById(this.currentUser.id).subscribe((data) => {this.user = data;}); 
+    this.user.id = this.currentUser.id ; 
+    
   }
-
+   EditCurrentUser(){
+    this.showConfirmationDialog = true;
+      this.authService.updateUser(this.user).subscribe(
+        (data) => {console.log('here updated user : ' , data);
+        this.authService.getCurrentUserById(this.currentUser.id).subscribe((data) => {this.user = data;}); 
+        this.update=true; 
+        setTimeout(() => {
+          this.update = false;
+        }, 3000); // 3000 ms = 3 secondes
+       },
+        (err) => {console.log('probleme !!! ' , err ) ;
+              this.erreur = true ; 
+              setTimeout(() => {
+                this.erreur = false;
+              }, 3000); // 3000 ms = 3 secondes
+        
+      }
+      )
+     
+      this.currentUser.firstName = this.user.nom ; 
+      this.currentUser.lastName = this.user.prenom ; 
+      this.currentUser.email = this.user.email ; 
+      this.token.saveUser(this.currentUser); 
+      this.closeConfirmationDialog(); 
+   }
+   closeConfirmationDialog(){
+    this.showConfirmationDialog = false;
+    this.authService.getCurrentUserById(this.currentUser.id).subscribe((data) => {this.user = data;}); 
+   }
+   openConfirmationDialog(){
+    this.showConfirmationDialog = true;
+   }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -8,17 +8,30 @@ import { Observable } from 'rxjs';
 export class AuthService {
 
   constructor(private http: HttpClient) { }
-  userURL: string = "http://localhost:8080/api/auth/";
+  userURL: string = "http://localhost:8080/api/auth";
   URL : string ="http://localhost:8080/utilisateurs/"
 
   login(user : any ) {
-    return this.http.post<{accessToken:any}>(this.userURL + "login", user);
+    return this.http.post<{accessToken:any}>(this.userURL + "/login", user);
   }
 
   register(user : any ): Observable<any> {
-    return this.http.post(this.userURL + "signup", user);
+    return this.http.post(this.userURL + "/signup", user);
   }
   forgetPassword (email :any) :Observable<boolean>{
     return this.http.get<boolean> (this.URL +"forgot-password/" +email);
+  }
+
+  updateUser(user: any): Observable<any> {
+    const url = `${this.userURL}/UpdateCurrentUser`;
+    return this.http.put<any>(url, user);
+  }
+ /* getCurrentUserById(id: number): Observable<any> {
+    const url = `${this.userURL}/${id}`;
+    return this.http.get<any>(url);
+  }*/
+ 
+  getCurrentUserById(id:number){
+    return this.http.get(`${this.userURL}/${id}`)
   }
 }
